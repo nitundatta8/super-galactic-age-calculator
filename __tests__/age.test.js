a043b9b742b59023e340131effec457588f27b8f
nitunwa <nitun.2@gmail.com>
2020-04-17 11:26:41 -0700

    test planet , earth

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,5 +1,26 @@
 import {Planet,Earth,Mercury,Venus,Mars} from "./../src/age-calculator";
 
 describe('Planet',function(){
+  let planet;
+  let earth;
+  beforeEach(function(){
+     planet = new Planet();
+     earth = new Earth();
+  });
 
+  test ('verifies planet is created', function() {
+      expect(planet.earthAgeFactor).toEqual(0);
+  });
+
+  test ('verifies earth is created', function() {
+      expect(earth.earthAgeFactor).toEqual(1);
+  });
+
+  // test ('verifies planet is created', function() {
+  //   expect(planet.earthAgeFactor).toEqual(0);
+  // });
+
+  // test ('verifies planet is created', function() {
+  //   expect(planet.earthAgeFactor).toEqual(0);
+  // });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,36 +1,39 @@
 export class Planet{
   constructor(){
-      this.earthAgeFactor = 1;
+      this.earthAgeFactor = 0;
   }
 }
 
 export class Earth extends Planet{
   constructor(){
    super();
-   this.earthAgeFactor =1;
+   this.earthAgeFactor = 1;
   }
 }
 export class Mercury extends Planet {
   constructor(){
     super();
-    this.earthAgeFactor =1;
+    this.earthAgeFactor =.24;
    }
 }
 
 export class Venus extends Planet {
-  constructor(earthAgeFactor){
-   super(earthAgeFactor);
-  }
+  constructor(){
+    super();
+    this.earthAgeFactor =.62;
+   }
 }
 export class Mars extends  Planet   {
-  constructor(earthAgeFactor){
-   super(earthAgeFactor);
-  }
+  constructor(){
+    super();
+    this.earthAgeFactor =1.88;
+   }
 }
 export class Jupiter extends Planet  {
-  constructor(earthAgeFactor){
-   super(earthAgeFactor);
-  }
+  constructor(){
+    super();
+    this.earthAgeFactor = 11.86;
+   }
 }
 
 // export class GalacticAgeCalculator  {
