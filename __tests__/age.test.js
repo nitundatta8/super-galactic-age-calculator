9c5c3f41b0f8b39298731cf1d24081127fa26920
nitunwa <nitun.2@gmail.com>
2020-04-17 12:01:33 -0700

    successfully test all planets

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,11 +1,21 @@
-import {Planet,Earth,Mercury,Venus,Mars} from "./../src/age-calculator";
+import {Planet,Earth,Mercury,Venus,Mars,Jupiter} from "./../src/age-calculator";
 
 describe('Planet',function(){
   let planet;
   let earth;
+  let mercury; 
+  let venus;
+  let mars;
+  let jupiter;
+   
   beforeEach(function(){
      planet = new Planet();
      earth = new Earth();
+     mercury = new Mercury();
+     venus = new Venus();
+     mars = new Mars();
+     jupiter = new Jupiter();
+     
   });
 
   test ('verifies planet is created', function() {
@@ -16,11 +26,20 @@ describe('Planet',function(){
       expect(earth.earthAgeFactor).toEqual(1);
   });
 
-  // test ('verifies planet is created', function() {
-  //   expect(planet.earthAgeFactor).toEqual(0);
-  // });
-
-  // test ('verifies planet is created', function() {
-  //   expect(planet.earthAgeFactor).toEqual(0);
-  // });
+  test ('verifies mercury is created', function() {
+    expect(mercury.earthAgeFactor).toEqual(.24);
+  });
+  
+  test ('verifies venus is created', function() {
+    expect(venus.earthAgeFactor).toEqual(.62 );
+  });
+  
+  test ('verifies mars is created', function() {
+    expect(mars.earthAgeFactor).toEqual(1.88 );
+  });
+  
+  test ('verifies jupiter is created', function() {
+    expect(jupiter.earthAgeFactor).toEqual(11.86 );
+  });
+  
 });
